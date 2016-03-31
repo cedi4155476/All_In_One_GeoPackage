3740ffc9bba28652fbe30fb200826853742ffc31
cedi <cedric_christen@bluewin.ch>
2016-03-31 14:07:57 +0200

    connection file edited and set up working environment

---
+++ read.py
@@ -0,0 +1,4 @@
+class Read():
+    def __init__(self, parent=None, iface):
+        self.parent = parent
+        self.iface = iface
---
+++ write.py
@@ -0,0 +1,4 @@
+class Write():
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.iface = iface
